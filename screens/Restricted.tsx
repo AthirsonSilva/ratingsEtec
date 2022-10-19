8ddf9b18918743b375cd691ed515fd528764dc76
Athirson <Athirsonarceus@gmail.com>
2022-10-19 14:57:16 -0300

    area restrita marromeno

--- screens/Restricted.tsx
+++ screens/Restricted.tsx
@@ -1,72 +1,65 @@
-import * as React from 'react'
+import React from 'react';
 import {
 	FlatList,
-	LogBox,
-	SafeAreaView,
 	Text,
-	TouchableOpacity,
 	View,
-} from 'react-native'
-import styles from '../styles'
+	ActivityIndicator,
+	TouchableOpacity,
+} from 'react-native';
+import { SearchBar } from 'react-native-elements';
 
-LogBox.ignoreLogs([
-	'Failed prop type: Invalid prop `textStyle` of type `array` supplied to `Cell`, expected `object`.',
-])
+import styles from '../styles';
 
-const RestrictedArea = () => {
-	const [data, setData] = React.useState<any[]>([])
-	const [loading, setLoading] = React.useState(true)
-	const [error, setError] = React.useState(false)
+export default function RestrictedPage({ navigation }: any) {
+	const [data, setData] = React.useState<any[]>([]);
+	const [filtered, setFiltered] = React.useState<any[]>([]);
+	const [search, setSearch] = React.useState<string>('');
+	const [loading, setLoading] = React.useState<boolean>(true);
 
-	const handleDelete = async () => {
-		await fetch('http://localhost:3000/ratings', {
-			method: 'DELETE',
-			headers: {
-				'Content-Type': 'application/json',
-				accept: 'application/json',
-			},
-		})
-			.then((response) => response.json())
-			.then((data) => console.table(data))
-			.catch((error) => console.error(error))
-	}
+	const searchFilterFunction = (text: string) => {
+		// Check if searched text is not blank
+		if (text) {
+			// Inserted text is not blank
+			// Filter the masterDataSource and update FilteredDataSource
+			const newData = data.filter((item) => {
+				// Applying filter for the inserted text in search bar
+				const itemData = item.data
+					? item.data.toUpperCase()
+					: ''.toUpperCase();
 
-	const handleGet = async (): Promise<void> => {
+				const textData = text.toUpperCase();
+
+				return itemData.indexOf(textData) > -1;
+			});
+			setFiltered(newData);
+			setSearch(text);
+		} else {
+			// Inserted text is blank
+			// Update FilteredDataSource with masterDataSource
+			setFiltered(data);
+			setSearch(text);
+		}
+	};
+
+	const getRatings = async () => {
 		await fetch('http://localhost:3000/ratings', {
 			method: 'GET',
 			headers: {
-				mode: 'no-cors',
 				'Content-Type': 'application/json',
-				accept: 'application/json',
+				Accept: 'application/json',
 			},
 		})
-			.then((response: any) => response.json())
-			.then((response: any) => {
-				response.forEach((item: any) => {
-					console.table(item)
+			.then((response) => response.json())
+			.then((json) => {				
+				json.forEach((rating: any) => {
+					data.push(rating);
+				});
 
-					data.push(item)
-				})
-
-				console.table(data)
+				console.table(data);
 			})
 			.catch((error) => console.error(error))
-	}
-
-	React.useEffect(() => {
-		handleGet()
-		const headers: any = {
-			id: 'ID',
-			subject: 'Subject',
-			rating: 'Rating',
-			positive: 'Positive',
-			negative: 'Negative',
-		}
-
-		setData(headers)
-
-		setLoading(false)
-	}, [])
+			.finally(() => setLoading(false));
+	};
 
 	const renderItem = ({ item }: { item: any }): JSX.Element => {
 		return (
@@ -76,7 +69,7 @@ const RestrictedArea = () => {
 					styles.row,
 					{
 						backgroundColor:
-							item.id === 'id' ? '#f9c2ff' : '#f6f6f6',
+							item.id === 'ID' ? '#f9c2ff' : '#f6f6f6',
 					},
 				]}
 			>
@@ -85,7 +78,7 @@ const RestrictedArea = () => {
 						styles.item,
 						{
 							backgroundColor:
-								item.id === 'id' ? '#f9c2ff' : '#f6f6f6',
+								item.id === 'ID' ? '#f9c2ff' : '#f6f6f6',
 						},
 					]}
 				>
@@ -93,10 +86,10 @@ const RestrictedArea = () => {
 						style={[
 							styles.listTitle,
 							{
-								fontSize: item.id === 'id' ? 28 : 24,
+								fontSize: item.id === 'ID' ? 28 : 24,
 								fontWeight:
-									item.id === 'id' ? 'bold' : 'normal',
-								color: item.id === 'id' ? '#000' : '#000',
+									item.id === 'ID' ? 'bold' : 'normal',
+								color: item.id === 'ID' ? '#000' : '#000',
 							},
 						]}
 					>
@@ -108,7 +101,7 @@ const RestrictedArea = () => {
 						styles.item,
 						{
 							backgroundColor:
-								item.id === 'id' ? '#f9c2ff' : '#f6f6f6',
+								item.id === 'ID' ? '#f9c2ff' : '#f6f6f6',
 						},
 					]}
 				>
@@ -116,40 +109,74 @@ const RestrictedArea = () => {
 						style={[
 							styles.listTitle,
 							{
-								fontSize: item.subject === 'SUBJECT' ? 28 : 24,
+								fontSize: item.data === 'DATA' ? 28 : 24,
 								fontWeight:
-									item.subject === 'SUBJECT'
-										? 'bold'
-										: 'normal',
-								color:
-									item.subject === 'SUBJECT'
-										? '#000'
-										: '#000',
+									item.data === 'DATA' ? 'bold' : 'normal',
+								color: item.data === 'DATA' ? '#000' : '#000',
 							},
 						]}
 					>
-						{item.subject}
+						{item.data}
 					</Text>
 				</View>
 			</View>
-		)
-	}
+		);
+	};
+
+	React.useEffect(() => {
+		getRatings();
+		const headers = {
+			id: 'ID',
+			subject: 'Subject',
+			rating: 'Rating',
+			teacher: 'Teacher',
+			student: 'Student',
+			positive: 'Positive',
+			negative: 'Negative',
+		};
+
+		data.push(headers);
+	}, []);
 
 	return (
-		<SafeAreaView style={styles.body}>
-			<FlatList
-				data={data}
-				renderItem={renderItem}
-				keyExtractor={(item) => item.id}
-			/>
+		<View style={[styles.container, { backgroundColor: '#fff' }]}>
+			{loading ? (
+				<ActivityIndicator />
+			) : (
+				<View style={styles.container}>
+					<SearchBar
+						placeholder="Search ratings..."
+						lightTheme
+						platform="android"
+						round
+						value={search}
+						onChangeText={(text: string) => searchFilterFunction(text)}
+						autoCorrect={false}
+						blurOnSubmit={true}
+						autoFocus={true}
+						style={{
+							width: '72%',
+						}}
+					/>
 
-			<View style={styles.lilMargin}>
-				<TouchableOpacity style={styles.button} onPress={handleDelete}>
-					<Text style={{ fontWeight: 'bold' }}>Apagar tudo</Text>
-				</TouchableOpacity>
-			</View>
-		</SafeAreaView>
-	)
+					<View style={[styles.row, { marginBottom: 15 }]}>
+						<TouchableOpacity
+							style={[styles.button]}
+							onPress={() => navigation.navigate('Form')}
+						>
+							<Text style={[styles.listTitle]}> Home </Text>
+						</TouchableOpacity>
+					</View>
+
+					<FlatList
+						data={filtered}
+						renderItem={renderItem}
+						keyExtractor={(item) => item.id}
+						scrollEnabled={true}
+						bounces={true}
+					/>
+				</View>
+			)}
+		</View>
+	);
 }
-
-export default RestrictedArea
